114b6f180017eca392a389fe635dd1a05ab6a50b
ghuma03 <lucasmarion.dev@gmail.com>
2025-11-22 18:24:05 -0300

    Conceitos básicos de funções, statements e expressions

---
+++ 2.concepts/functions/target/debug/deps/functions-631f332feca23877.d
@@ -0,0 +1,5 @@
+/home/lucas/Área de trabalho/dev_area/rust/2.concepts/functions/target/debug/deps/functions-631f332feca23877.d: src/main.rs
+
+/home/lucas/Área de trabalho/dev_area/rust/2.concepts/functions/target/debug/deps/functions-631f332feca23877: src/main.rs
+
+src/main.rs:
---
+++ 2.concepts/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/lucas/Área\ de\ trabalho/dev_area/rust/2.concepts/functions/target/debug/functions: /home/lucas/Área\ de\ trabalho/dev_area/rust/2.concepts/functions/src/main.rs
